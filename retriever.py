ccc40ef6661b0291428d5d568cbdb4f27a656d47
AsRumi <mohammedmutahar3@gmail.com>
2026-03-30 18:09:11 -0400

    Retriever written. Reranker to be written.

--- retriever.py
+++ retriever.py
@@ -4,3 +4,16 @@ Retriever to get data from a vector store.
 
 from ingest import documents
 from ingest import model
+from sentence_transformers import CrossEncoder
+
+reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+def retrieve(query, k: int):
+    query_embedding = model.encode([query]).tolist()
+    similar_embeddings = documents.query(query_embeddings = query_embedding,
+                    n_results = k)
+    return similar_embeddings
+
+query = "What building materials did early humans use?"
+results = retrieve(query, 3)
+print(results["documents"])
